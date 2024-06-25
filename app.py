75f8195b63512fd9e302a62fefec6406077c387e
HudsonArauj <hudsonma@al.insper.edu>
2024-06-25 20:58:05 -0300

    Feat:Page login

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from dotenv import load_dotenv
 import streamlit as st
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities.hasher import Hasher
+import yaml
+import streamlit as st
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities.exceptions import (CredentialsError,
+                                                          ForgotError,
+                                                          LoginError,
+                                                          RegisterError,
+                                                          ResetError,
+                                                          UpdateError) 
+
 import os
 
 # Langchain
@@ -14,6 +27,7 @@ from operator import itemgetter
 from pathlib import Path
 import base64
 
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,6 +41,7 @@ def cs_sidebar():
 
     # st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=128 height=128>](https://www.upload.ventures/)'''.format(img_to_bytes("imgs/upload_logo.jpg")), unsafe_allow_html=True)
     st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=112 height=20>](https://neroai.com.br/)'''.format(img_to_bytes("imgs/neroai_logo.png")), unsafe_allow_html=True)
+    st.sidebar.markdown(f'Bem-vindo *{st.session_state["name"]}*')
     st.sidebar.header('Powered by [Nero.AI](https://neroai.com.br/)')
 
     return None
@@ -61,6 +76,7 @@ def respond(user_query, chat_history, db, retriever):
                     Se apresente e diga como você pode ajudar."""),
         ('system', "Aqui está o contexto adicional de videos no YouYube:\n\n{context_query}\n\nSempre que possível, cite fontes (dados do YouTube) de onde você está tirando a informação. Somente cite fontes dos documentos fornecidos acima."),
         ('system', "Obrigatoriamente tente relacionar o contexto da adicional com o contexto da pergunta. FORNEÇA LINKS para conteúdos que possam interessar ao usuários. JAMAIS USE LINKS QUE NÂO FORAM FORNECIDOS A VOCÊ."),
+        ('system', "Responda as perguntas com uma linguagem Markdown"),
         ('system', "{query}"),
     ]
     
@@ -87,42 +103,70 @@ embedding_size = 1536
 embedding_model = 'text-embedding-ada-002'
 embeddings = OpenAIEmbeddings(model=embedding_model)
 
-# app config
-st.set_page_config(page_title="Edu Bot", page_icon="imgs/perfil.png")
-st.title("Assistente virtual - Pergunte ao EduBot")
-cs_sidebar()
 
-# session state
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content="Olá, eu sou o Edu Bot que foi desenvolvido pela Nero.AI. Estou aqui para responder perguntas sobre ENEM. Como posso ajudar você?"),
-    ]
 
-if 'db' not in st.session_state:
-    st.session_state.db = vector_db()
-    st.session_state.retriever = st.session_state.db.as_retriever(search_kwargs={"k": 3})
+# Carrega o arquivo config
+with open('config.yaml', 'r', encoding='utf-8') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-# conversation
-for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
-        with st.chat_message("AI", avatar="imgs/perfil.png"):
-            st.write(message.content)
-    elif isinstance(message, HumanMessage):
-        with st.chat_message("Human", avatar="👤"):
-            st.write(message.content)
+# Cria o authenticator 
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
 
-# user input
-user_query = st.chat_input("Type your message here...")
-if user_query is not None and user_query != "":
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
+# Cria a tela
+try:
+    authenticator.login()
+except LoginError as e:
+    st.error(e)
 
-    with st.chat_message("Human", avatar="👤"):
-        st.markdown(user_query)
-
+if st.session_state["authentication_status"]: #Se as credenciais forem corretas abre o chatbot
+    authenticator.logout(location='sidebar')
     
-    with st.chat_message("AI", avatar="imgs/perfil.png"):
-        with st.spinner("Thinking..."):
-            response = st.write_stream(respond(user_query, st.session_state.chat_history, st.session_state.db, st.session_state.retriever))
 
-    st.session_state.chat_history.append(AIMessage(content=response))
+    #CHATBOT
+    st.title("Assistente virtual - Pergunte ao EduBot")
+    cs_sidebar()
+    
 
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Olá, eu sou o Edu Bot que foi desenvolvido pela Nero.AI. Estou aqui para responder perguntas sobre ENEM. Como posso ajudar você?"),
+        ]
+
+    if 'db' not in st.session_state:
+        st.session_state.db = vector_db()
+        st.session_state.retriever = st.session_state.db.as_retriever(search_kwargs={"k": 3})
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI", avatar="imgs/perfil.png"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human", avatar="👤"):
+                st.write(message.content)
+
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+        with st.chat_message("Human", avatar="👤"):
+            st.markdown(user_query)
+
+        
+        with st.chat_message("AI", avatar="imgs/perfil.png"):
+            with st.spinner("Thinking..."):
+                response = st.write_stream(respond(user_query, st.session_state.chat_history, st.session_state.db, st.session_state.retriever))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+elif st.session_state["authentication_status"] is False: # Se as credenciais forem falsas, gera o aviso
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
