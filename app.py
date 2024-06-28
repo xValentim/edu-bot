ab078532c7f7879d4c78785b8d34a8069b5f2753
HudsonArauj <hudsonma@al.insper.edu>
2024-06-28 19:13:09 -0300

    Fix:Bug navigation screens

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ from streamlit_authenticator.utilities.exceptions import (CredentialsError,
                                                           RegisterError,
                                                           ResetError,
                                                           UpdateError) 
-import streamlit as st
-from utils import*
-
+from utils import *
 
 def reset_conversation():
-  st.session_state.chat_history = None
-
+    st.session_state.chat_history = None
 
 # Cria o authenticator 
 authenticator = stauth.Authenticate(
@@ -24,59 +21,69 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['pre-authorized']
 )
+
 # LOGIN
 try:
-    authenticator.login()
+    authenticator.login(clear_on_submit=True)
 except LoginError as e:
     st.error(e)
-    
+
 if 'authentication_status' not in st.session_state:
     st.session_state['authentication_status'] = None
 
-if st.session_state["authentication_status"]: #Se as credenciais forem corretas abre o chatbot
+if st.session_state["authentication_status"]: # Se as credenciais forem corretas abre o chatbot
     authenticator.logout(location='sidebar')
-    
-    #CHATBOT
-    st.title("Assistente virtual - Pergunte ao EduBot")
     cs_sidebar()
+    # Título do aplicativo
+    st.title("Assistente virtual - Pergunte ao EduBot")
 
-    # session state
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            AIMessage(content="Olá, eu sou o Edu Bot que foi desenvolvido pela Nero.AI. Estou aqui para responder perguntas sobre ENEM. Como posso ajudar você?"),
-        ]
 
-    if 'db' not in st.session_state:
-        st.session_state.db = vector_db()
-        st.session_state.retriever = st.session_state.db.as_retriever(search_kwargs={"k": 3})
+    page = st.sidebar.selectbox(
+        "Navegar para:",
+        ["Chatbot", "Redação"]
+    )
 
-    # conversation
-    for message in st.session_state.chat_history:
-        if isinstance(message, AIMessage):
-            with st.chat_message("AI", avatar="imgs/perfil.png"):
-                st.write(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message("Human", avatar="👤"):
-                st.write(message.content)
-
-    # user input
-    user_query = st.chat_input("Type your message here...")
-    if user_query is not None and user_query != "":
-        st.session_state.chat_history.append(HumanMessage(content=user_query))
-
-        with st.chat_message("Human", avatar="👤"):
-            st.markdown(user_query)
-
+    if page == "Chatbot":
+        st.subheader("Chatbot")
         
-        with st.chat_message("AI", avatar="imgs/perfil.png"):
-            with st.spinner("Thinking..."):
-                response = st.write_stream(respond(user_query, st.session_state.chat_history, st.session_state.db, st.session_state.retriever))
-        st.session_state.chat_history.append(AIMessage(content=response))
+        if "chat_history" not in st.session_state:
+            st.session_state.chat_history = [
+                AIMessage(content="Olá, eu sou o Edu Bot que foi desenvolvido pela Nero.AI. Estou aqui para responder perguntas sobre ENEM. Como posso ajudar você?"),
+            ]
+
+        if 'db' not in st.session_state:
+            st.session_state.db = vector_db()
+            st.session_state.retriever = st.session_state.db.as_retriever(search_kwargs={"k": 3})
+
+        for message in st.session_state.chat_history:
+            if isinstance(message, AIMessage):
+                with st.chat_message("AI", avatar="imgs/perfil.png"):
+                    st.write(message.content)
+            elif isinstance(message, HumanMessage):
+                with st.chat_message("Human", avatar="👤"):
+                    st.write(message.content)
+
+        # Entrada do usuário
+        user_query = st.chat_input("Digite sua mensagem aqui...")
+        if user_query is not None and user_query != "":
+            st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+            with st.chat_message("Human", avatar="👤"):
+                st.markdown(user_query)
+
+            with st.chat_message("AI", avatar="imgs/perfil.png"):
+                with st.spinner("Pensando..."):
+                    response = st.write_stream(respond(user_query, st.session_state.chat_history, st.session_state.db, st.session_state.retriever))
+            st.session_state.chat_history.append(AIMessage(content=response))
+
+    elif page == "Redação":
+        st.subheader("Redação")
+        pg = st.navigation([st.Page("./pages/redacao.py")])
+        pg.run()
 
 elif st.session_state["authentication_status"] is False: # Se as credenciais forem falsas, gera o aviso
     st.error('Username/password is incorrect') 
 elif st.session_state["authentication_status"] is None:
     if "chat_history" in st.session_state:
         st.session_state.pop("chat_history")
-
     st.warning('Please enter your username and password')
